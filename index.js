462d6ca6c82c6249d839c9e4bcb351f283f7e63d
bitsybaron <59240206+bitsybaron@users.noreply.github.com>
2020-08-05 15:52:37 -0600

    working on error

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+require('dotenv').config();
 const express = require("express");
+const massive = require('massive');
 
 const app = express();
 
-const { SERVER_PORT } = process.env;
-
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+massive({
+  connectionString:  CONNECTION_STRING,
+  ssl: {
+    rejectUnathorized: false
+  }
+}).then(db => {
+  app.set("db", db)
+  console.log('db connected')
+}).catch(err => console.log(err));
 app.use(express.json());
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening on port ${SERVER_PORT}`);
 });
+
